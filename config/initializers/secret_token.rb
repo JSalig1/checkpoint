3dca1f7f0c64c405656916278dc1124046cc1db5
jsalig1 <jeremy.salig@gmail.com>
2014-04-28 14:19:41 -0400

    Create user accounts with clearance

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Checkpoint::Application.config.secret_key_base = 'cfa43c0644cc920f437d069999d413ab4254747e01f3dca17a49cd53a08432e65121d4f63ddc3b3a54ebc114b0c1fe91e6566014abba841a8a523b892dd456bf'
