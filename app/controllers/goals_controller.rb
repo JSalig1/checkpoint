f37696d3464a01668c2a8662f4c8c5389d7cbee4
jsalig1 <jeremy.salig@gmail.com>
2014-04-29 15:28:28 -0400

    Create and view list of goals

---
+++ app/controllers/goals_controller.rb
@@ -0,0 +1,24 @@
+class GoalsController < ApplicationController
+
+  def index
+    @goals = current_user.goals
+  end
+
+  def new
+    @goal = Goal.new
+  end
+
+  def create
+    @goal = current_user.goals.new(goal_params)
+    if !@goal.save
+      render :new
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:description, :target_date, :status)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428180657) do
+ActiveRecord::Schema.define(version: 20140428194629) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goals", force: true do |t|
+    t.integer  "user_id"
+    t.text     "description",                                 null: false
+    t.date     "target_date",                                 null: false
+    t.string   "status",      limit: 128, default: "current", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "goals", ["user_id"], name: "index_goals_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.datetime "created_at",                     null: false
