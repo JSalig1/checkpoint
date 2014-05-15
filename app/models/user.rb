31cf2ad133d9af390f8027e48306d19a348c53f3
jsalig1 <jeremy.salig@gmail.com>
2014-05-15 09:58:25 -0400

    styling and minor functionality fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,17 @@ class User < ActiveRecord::Base
   has_many :goals, dependent: :destroy
   has_many :steps, through: :goals
 
-  has_many :coaching_relationships,
+  has_many :coach_to_athlete_relationships,
     foreign_key: :coach_id,
+    class_name: "CoachingRelationship",
     dependent: :destroy
-  has_many :athletes, through: :coaching_relationships
+  has_many :athletes, through: :coach_to_athlete_relationships
 
-  has_many :coaching_relationships,
+  has_many :athlete_to_coach_relationships,
     foreign_key: :athlete_id,
+    class_name: "CoachingRelationship",
     dependent: :destroy
-  has_many :coaches, through: :coaching_relationships
+  has_many :coaches, through: :athlete_to_coach_relationships
 
   has_many :review_requests, foreign_key: :athlete_id
   has_many :review_requests, foreign_key: :coach_id
@@ -33,14 +35,26 @@ class User < ActiveRecord::Base
   end
 
   def coach(athlete)
-    coaching_relationships.create(athlete: athlete)
+    coach_to_athlete_relationships.create(athlete_id: athlete.id)
   end
-  
+
   def coaching?(athlete)
     athlete_ids.include? athlete.id
   end
-  
+
   def uncoach(athlete)
     athletes.destroy athlete
+  end
+
+  def has_coaches?
+    coaches != []
+  end
+
+  def coaches_athletes?
+    athletes != []
+  end
+
+  def has_coach_reviews?
+    review_requests != []
   end
 end
