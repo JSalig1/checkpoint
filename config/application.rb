9749d0f9829ace838d0cd18fe66b9ceb19f741b7
jsalig1 <jeremy.salig@gmail.com>
2014-06-02 12:11:19 -0400

    precompile command removed from application.rb

--- config/application.rb
+++ config/application.rb
@@ -2,10 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.precompile += [
-      'application.css.scss'
-    ]
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
