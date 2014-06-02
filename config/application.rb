f6c94b97d8e609c4eec9b5124559b97e0371e641
jsalig1 <jeremy.salig@gmail.com>
2014-06-02 11:30:29 -0400

    rails_12factor installed for deployment

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.precompile += [
+      'application.css.scss'
+    ]
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
